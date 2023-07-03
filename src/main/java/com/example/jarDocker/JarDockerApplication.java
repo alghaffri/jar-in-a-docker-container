80fc167b5b95a52e7bdc9269c400fa0f3abf146e
alghaffri <isami.abdullah@gmail.com>
2023-07-03 11:15:39 +0400

    first commit

---
+++ src/main/java/com/example/jarDocker/JarDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jarDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jarDocker/JarDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jarDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
